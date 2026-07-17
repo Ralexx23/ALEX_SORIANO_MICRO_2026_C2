9b371a75e91a09b296af0c5991187dd2815f0afe
Alex Soriano <106250241+Ralexx23@users.noreply.github.com>
2026-07-16 20:11:47 -0400

    Delete Tarea 4 y 5 directory

--- Tarea 4 y 5/main/app_main.c
+++
@@ -1,8 +0,0 @@
-#include "control_task.h"
-
-void app_main(void)
-{
-    control_task_start();
-    /* app_main puede retornar; la tarea de control sigue corriendo en
-     * su propio contexto FreeRTOS. */
-}
--- Tarea 4 y 5/main/control_task.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-/* Inicializa todos los subsistemas (ADC, botones, IMU, display, RF,
- * WiFi/MQTT) y lanza la tarea FreeRTOS que corre el ciclo principal:
- * leer -> fusionar joystick+giroscopio -> armar trama -> transmitir RF
- * -> refrescar pantalla -> publicar MQTT. */
-void control_task_start(void);
